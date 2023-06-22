1da717df49a096dc59d02cb76630204a36b05ed3
DadasoBangar <dadasobanagar0906@gmail.com>
2023-06-22 12:58:34 +0530

    Added hystrixdashboard service source code

---
+++ src/main/java/com/hystrix/microservice/HystrixServiceApplication.java
@@ -0,0 +1,17 @@
+package com.hystrix.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableHystrixDashboard
+public class HystrixServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixServiceApplication.class, args);
+	}
+
+}
